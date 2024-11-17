ef1d8914a39e13ef6b77ba9c9f357c1a20e07f19
jlpach <jlpach@gmail.com>
2024-11-17 02:15:59 -0600

    Carga de archivos finales

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import cartContext from "./CartContext"
+import { cartContext } from "./CartContext"
 import { useState } from "react"
 
 function CartProvider({ children }) {
